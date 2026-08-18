c5ede9b9fdb4130984f3c31ad81611b9202af84b
Theo-b-col <tb3277@columbia.edu>
2026-08-18 16:23:06 -0400

    Added duration convexity and yield sensitivity analysis

--- src/main.py
+++ src/main.py
@@ -8,6 +8,7 @@ bond = bond(
     compoundingfreq=2 
 )
 
+
 print("Bond Created")
 print("----------------")
 print("Face Value:", bond.face_value)
@@ -22,3 +23,24 @@ pricing = BondPricing(bond)
 print(f"YTM = 5% --> price = {pricing.bond_pricer(0.05):.2f}")
 print(f"YTM = 7%  --> Price = {pricing.bond_pricer(0.07):.2f}")
 print(f"YTM = 3%  --> Price = {pricing.bond_pricer(0.03):.2f}")
+
+print(pricing.valuation_table(.05))
+print(f"Macaulay Duration: {pricing.duration(.05):.2f}")
+print(f"Modified Duration: {pricing.modified_duration(.05):.2f}")
+print(f"Convexity: {pricing.convexity(.05):.2f}")
+
+yields = [.02,.03,.04,.05,.06,.07,.08]
+print("Yield Sensitivity:")
+print(pricing.yield_sensitivity(yields))
+
+shocks = [-0.02, -0.01, -0.005, 0.005, 0.01, 0.02]
+print("\n Yield Shock Analysis:")
+for shock in shocks: 
+    result = pricing.yield_shock_analysis(.05, .05 + shock)
+    print(
+        f"Shock: {shock:+.2%} | "
+        f"Actual: {result['Actual Change']:+.2%} | "
+        f"Duration: {result['Duration Approximation']:+.2%} | "
+        f"Duration + Convexity: "
+        f"{result['Duration + Convexity Approximation']:+.2%}"
+    )
--- src/pricing.py
+++ src/pricing.py
@@ -1,3 +1,5 @@
+import pandas as pd 
+
 class BondPricing: 
 
     def __init__(self, bond):
@@ -10,6 +12,18 @@ class BondPricing:
         periodic_ytm = ytm / self.bond.compoundingfreq
         return 1 / (1+periodic_ytm) ** period
 
+    def valuation_table(self, ytm):
+        cashflows = self.bond.cash_flows()
+        cashflows["Discount Factor"] = [
+            self.discount_factor(ytm, period) for period in cashflows["Period"]
+        ]
+        cashflows["Present Value"] = (
+                cashflows["Cash Flow"] * cashflows["Discount Factor"]
+            )
+        
+
+        return cashflows
+    
     def bond_pricer(self, ytm):
         """
         Prices a Fixed-rate bond using discounted cash flows 
@@ -20,16 +34,91 @@ class BondPricing:
         Returns:
             Present value of the bond
         """
-        price = 0 
-        cashflows = self.bond.cash_flows()
-        for _, row in cashflows.iterrows(): 
-            period = row["Period"]
-            cashflow = row["Cash Flow"]
-            
-            discount_factor = self.discount_factor(ytm, period)
-            
-            present_value = cashflow * discount_factor
-            price += present_value
-        return price
+        table = self.valuation_table(ytm)
+        return table["Present Value"].sum()
         
+    def yield_sensitivity(self,yields):
+        """
+        Calculates the price of the bond under different yield curve scenarios 
+
+        Parameters: List of annual yields
+        returns: DataFrame containing YTM and bond price
+        """
+        prices = [] 
+        for ytm in yields:
+            price = self.bond_pricer(ytm)   
+            prices.append(price)
+        return pd.DataFrame({
+            "YTM": yields,
+            "Price": prices
+        })
+
+    def duration(self,ytm):
+        """
+        Calculates the Macaulay duration of a bond. 
+        """
+        table = self.valuation_table(ytm)
+
+        price = table["Present Value"].sum()
+        table["Duration Weight"] = (
+            table["Period"] * table["Present Value"]
+        )
+        duration = table["Duration Weight"].sum() / price
+
+        return duration/self.bond.compoundingfreq
     
+    def modified_duration(self, ytm):
+        """
+        Calculates modified duration
+        """
+        macaulay = self.duration(ytm)
+
+        periodic_ytm = ytm / self.bond.compoundingfreq
+        return macaulay / (1+periodic_ytm)
+    
+    def convexity(self, ytm): 
+        """
+        Calculates convexity of fixed-rate bond(second order effect)
+        """
+        table = self.valuation_table(ytm)
+        price = table["Present Value"].sum()
+        periodic_ytm = ytm / self.bond.compoundingfreq
+        periods = table["Period"]
+
+        table["Convexity Weight"] = (
+            periods * (periods + 1) * table["Present Value"]
+        )
+
+        convexity = ( 
+            table["Convexity Weight"].sum() / 
+            (price * (1+periodic_ytm) ** 2)
+        )
+
+        return convexity / self.bond.compoundingfreq ** 2
+    
+    def yield_shock_analysis(self, base_ytm, shocked_ytm):
+        """
+        Compares the actual bond price with duration and convextiy adjustments 
+        """
+
+        base_price = self.bond_pricer(base_ytm)
+        shocked_price = self.bond_pricer(shocked_ytm)
+
+        actual_change = (shocked_price - base_price) / base_price
+
+        delta = shocked_ytm - base_ytm
+        modified_duration = self.modified_duration(base_ytm)
+        convexity = self.convexity(base_ytm)
+
+        duration_approximation = -modified_duration * delta
+        convexity_approximation = ( 
+            duration_approximation + .5 * convexity * delta **2
+        )
+        
+        return { 
+            "Base Price": base_price,
+            "Shocked Price": shocked_price,
+            "Actual Change": actual_change,
+            "Duration Approximation": duration_approximation,
+            "Duration + Convexity Approximation": convexity_approximation
+        }
